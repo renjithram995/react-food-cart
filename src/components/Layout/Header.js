dfbba5b2848f56811f5e7b027229753ec62574c7
Renjith Ramaswamy <rrenjith1995@gmail.com>
2022-02-16 14:29:39 +0530

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react'
+import classes from './Header.module.css'
+import meals from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = () => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton>Your cart</HeaderCartButton>
+        </header>
+        <div className={classes['main-image']} >
+            <img src={meals} alt="A table full od delicious food!" />
+        </div>
+    </Fragment>
+}
+
+export default Header;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,17 @@
+import DUMMY_MEALS from "./dummyMeals.json";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} /> );
+  return (
+    <section className={classes.meals}>
+        <Card>
+            <ul>{mealsList}</ul>
+        </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
